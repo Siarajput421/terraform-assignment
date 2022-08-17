855b2f666bbf1404f2095e31d9a68d048805660b
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-08-17 18:39:17 +0530

    Delete terraform Assignment.tf directory

--- terraform Assignment.tf/natgateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_nat_gateway" "natgateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.subnet-1.id
-  tags = {
-    Name = "natgateway"
-  }
-}
--- terraform Assignment.tf/privateroutetableassociation.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route_table_association" "associate-Private" {
-  subnet_id      = aws_subnet" "subnet-2.id
-  route_table_id = aws_route_table.route-table-1.id
-}
