cb7a8596bab00eaa6501d2569e7b4c46b3be7b28
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-08-23 12:25:24 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "Default"
-  secret_key = "Q4pQ6fn2WENCwqKJyGIz3gzU5yCCRpszYszuUftG"
-} 
+provider aws {
+  profile = "default"
+  region  = "ap-south-1"
+}
+Footer
