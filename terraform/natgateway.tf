8e953ccf39fa1912da0cc6b26c38db02a52af1f4
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-08-23 14:43:58 +0530

    Update natgateway.tf

--- terraform/natgateway.tf
+++ terraform/natgateway.tf
@@ -1,7 +1,7 @@
 #natgatway
 resource "aws_nat_gateway" "myNatGateway" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id         = aws_subnet.public.id
+  subnet_id         = aws_subnet.terrapublicpublic.id
   tags = {
     Name = "myNatGateway"
   }
